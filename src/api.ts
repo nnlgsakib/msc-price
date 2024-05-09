8e29f0f0420e599785727617880cb4d840353323
nnlgsakib <nlgarts@outlook.com>
2024-05-09 10:37:16 +0600

    init

---
+++ src/api.ts
@@ -0,0 +1,1 @@
+export const PRICE_API : string = "https://mindchain.info/Api/Index/singlemarketInfo/market/mind_usdt" ;
---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from 'express';
+import axios from 'axios';
+import { PRICE_API } from './api';
+
+const app = express();
+const PORT = 3000;
+
+// Define a route to fetch and return the price
+app.get('/price', async (req: Request, res: Response) => {
+  try {
+    const response = await axios.get(PRICE_API);
+    const price = response.data.data.new_price;
+    const change = response.data.data.change;
+   res.json({ price , change}) ;
+  } catch (error) {
+    console.error('Error fetching price:', error);
+    res.status(500).json({ error: 'Failed to fetch price' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});

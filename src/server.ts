7036e891677acd6cabfa86c1459762ad9eb897b4
NLG Sakib <107909889+nnlgsakib@users.noreply.github.com>
2024-05-11 22:54:55 +0600

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response } from 'express';
 import axios from 'axios';
+import cors from 'cors'; // Import cors middleware
 import { PRICE_API } from './api';
 
 const app = express();
 const PORT = 3000;
+
+// Enable CORS for all origins
+app.use(cors());
 
 // Define a route to fetch and return the price
 app.get('/price', async (req: Request, res: Response) => {
@@ -11,7 +15,7 @@ app.get('/price', async (req: Request, res: Response) => {
     const response = await axios.get(PRICE_API);
     const price = response.data.data.new_price;
     const change = response.data.data.change;
-   res.json({ price , change}) ;
+    res.json({ price, change });
   } catch (error) {
     console.error('Error fetching price:', error);
     res.status(500).json({ error: 'Failed to fetch price' });
